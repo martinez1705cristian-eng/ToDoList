14faab62b1a9bdeb81e1c976982730430f9a3761
Cristian Martinez <martinez.1705.cristian@gmail.com>
2026-07-30 00:17:10 -0500

    add style button login, edit filter mode registered

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -7,63 +7,82 @@ const closeErrorLogin = document.getElementById("closeErrorLogin");
 const titleErrorLogin = document.getElementById("titleErrorLogin");
 const messageErrorLogin = document.getElementById("messageErrorLogin");
 const iconErrorLogin = document.getElementById("iconErrorLogin");
+const loginText = document.getElementById("loginText");
+const loginSpinner = document.getElementById("loginSpinner");
 
 closeErrorLogin.addEventListener("click", () => {
-	modalErrorLogin.classList.add("hidden");
-	modalErrorLogin.classList.remove("flex");
+  modalErrorLogin.classList.add("hidden");
+  modalErrorLogin.classList.remove("flex");
 });
 
 eyeIcon.addEventListener("click", () => {
-	eyeIcon.classList.toggle("fa-eye");
-	eyeIcon.classList.toggle("fa-eye-slash");
-	password.type = password.type === "password" ? "text" : "password";
+  eyeIcon.classList.toggle("fa-eye");
+  eyeIcon.classList.toggle("fa-eye-slash");
+  password.type = password.type === "password" ? "text" : "password";
 });
 
 function showErrorModal(title, message, isWarning = false) {
-	titleErrorLogin.textContent = title;
-	messageErrorLogin.textContent = message;
-	iconErrorLogin.className = isWarning
-		? "fa-solid fa-triangle-exclamation text-yellow-500 text-3xl"
-		: "fa-solid fa-circle-exclamation text-red-600 text-3xl";
-	modalErrorLogin.classList.remove("hidden");
-	modalErrorLogin.classList.add("flex");
+  titleErrorLogin.textContent = title;
+  messageErrorLogin.textContent = message;
+  iconErrorLogin.className = isWarning
+    ? "fa-solid fa-triangle-exclamation text-yellow-500 text-3xl"
+    : "fa-solid fa-circle-exclamation text-red-600 text-3xl";
+  modalErrorLogin.classList.remove("hidden");
+  modalErrorLogin.classList.add("flex");
 }
 
 function mapLoginError(status, message) {
-	if (status === 429) {
-		showErrorModal("Demasiados intentos", message, true);
-	} else if (message === "El usuario no existe") {
-		showErrorModal("Usuario no encontrado", message);
-	} else if (message === "Contraseña inválida") {
-		showErrorModal("Contraseña incorrecta", message);
-	} else if (message === "Error interno del servidor") {
-		showErrorModal("Error del servidor", message);
-	} else {
-		showErrorModal("Datos inválidos", message, true);
-	}
+  if (status === 429) {
+    showErrorModal("Demasiados intentos", message, true);
+  } else if (message === "El usuario no existe") {
+    showErrorModal("Usuario no encontrado", message);
+  } else if (message === "Contraseña inválida") {
+    showErrorModal("Contraseña incorrecta", message);
+  } else if (message === "Error interno del servidor") {
+    showErrorModal("Error del servidor", message);
+  } else {
+    showErrorModal("Datos inválidos", message, true);
+  }
 }
 
 async function sendDataUser() {
-	try {
-		const response = await axios.post(`${CONFIG.API_URL}/login`, {
-			username: username.value,
-			password: password.value,
-		});
-		username.value = "";
-		password.value = "";
-		if (response.data.success) {
-			localStorage.setItem("token", response.data.token);
-			window.location.replace("app.html");
-		}
-	} catch (error) {
-		if (error.response) {
-			mapLoginError(error.response.status, error.response.data.message);
-			password.value = "";
-			password.focus();
-		}
-	}
+  event.preventDefault(); // Si es un formulario
+
+  // 1. DESACTIVAR BOTÓN Y MOSTRAR SPINNER
+  loginButton.disabled = true;
+  loginButton.classList.add("opacity-50", "cursor-not-allowed");
+  loginText.textContent = "Cargando...";
+  loginSpinner.classList.remove("hidden");
+
+  
+  try {
+    const response = await axios.post(`${CONFIG.API_URL}/login`, {
+      username: username.value,
+      password: password.value,
+    });
+    username.value = "";
+    password.value = "";
+    if (response.data.success) {
+      localStorage.setItem("token", response.data.token);
+      window.location.replace("app.html");
+    }
+  } catch (error) {
+    if (error.response) {
+      mapLoginError(error.response.status, error.response.data.message);
+      password.value = "";
+      password.focus();
+    }
+
+	loginButton.disabled = false;
+    loginButton.classList.remove("opacity-50", "cursor-not-allowed");
+    loginText.textContent = "Iniciar Sesión";
+    loginSpinner.classList.add("hidden");
+    
+    showServerErrorModal(error);
+	
+  }
 }
 
 window.onload = () => {
-	username.focus();
+  username.focus();
 };
